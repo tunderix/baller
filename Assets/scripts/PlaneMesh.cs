51cb1e70efccc931ff2fff52d292ebf7a5dd2fbd
tunderix <sami.repo.koti@gmail.com>
2015-07-16 23:08:49 +0300

    BallerProject ON GIT

---
+++ Assets/scripts/PlaneMesh.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneMesh : MonoBehaviour {
+	public int shaken = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
