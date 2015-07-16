e90632bd2ed424b2b90beaeb87df75f024f75c7c
tunderix <sami.repo.koti@gmail.com>
2015-07-16 23:40:20 +0300

    Upping!

--- Assets/scripts/PlaneMesh.cs
+++ Assets/scripts/PlaneMesh.cs
@@ -5,7 +5,9 @@ public class PlaneMesh : MonoBehaviour {
 	public int shaken = 0;
 	// Use this for initialization
 	void Start () {
-	
+		//Can you see this?
+
+		//Can you saaaaaat
 	}
 	
 	// Update is called once per frame
